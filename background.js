17eaeb3cf3bd0792c29675019197d84681820081
Xisuthros <xisuthros@utnapisht.im>
2024-09-30 22:28:16 +0900

    remove recom cookie automatically

--- background.js
+++ background.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const re = /[a-z0-9]+_recom_[0-9]+/;
+
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
   let now = Date.now();
 
   browser.cookies.getAll({ domain: "dcinside.com" }).then((cookies) => {
     let promises = [];
     cookies.forEach((cookie) => {
-      if (!cookie.session && cookie.expirationDate * 1000 < now) {
+      if (!cookie.session && (cookie.expirationDate * 1000 < now || re.test(cookie.name))) {
         promises.push(browser.cookies.remove({ name: cookie.name, url: ((cookie.secure) ? 'https://' : 'http://') + cookie.domain + cookie.path }));
       }
     });
